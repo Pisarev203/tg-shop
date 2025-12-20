13c67fdffb82b0a4076e0e1972e0d679281997cc
Pisarev203 <mpisarev203@gmail.com>
2025-12-21 00:09:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from pathlib import Path
 import db
 
 app = FastAPI()
+
+BASE_DIR = Path(__file__).resolve().parent
+
 db.init_db()
 
 @app.get("/")
 def index():
-    return FileResponse("index.html")
+    return FileResponse(BASE_DIR / "index.html")
 
 @app.get("/api/products")
 def api_products():
