a52a2d6e0009dc3ffe1798e735ff699698eae523
Pisarev203 <mpisarev203@gmail.com>
2026-02-27 00:25:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-python-3.11.9
 import os, json
 from aiogram import Bot, Dispatcher, executor, types
 import db
@@ -38,4 +37,5 @@ async def order(m):
     await m.answer("Заказ принят")
 
 executor.start_polling(dp)
+
 
